b97742ced5e73e149804435920580478378fcf4b
phyatur <phyatur.official@gmail.com>
2023-02-22 05:11:44 +0000

    Task 11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,20 @@
 
 void print_to_98(int n)
 {
-	while (n < 98)
+	if (n > 98)
 	{
-		printf("%d,", n);
-		n++;
+	for (; n > 98; n--)
+		{
+		printf("%d, ", n);
+		}
 	}
-
-	while (n > 98)
+	else if (n < 98)
 	{
-		printf("%d,", n);
-		n--;
+		for (; n < 98; n++)
+		{
+		printf("%d, ", n);
+		}
 	}
-
-	if (n == 98)
-		printf("%d", n);
-
-	printf("\n");
+	printf("%d\n", n);
 }
+
