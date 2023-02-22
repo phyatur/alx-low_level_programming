feadb1a84d0a84ff4c25ab6d6eaf086c35ded680
phyatur <phyatur.official@gmail.com>
2023-02-22 04:51:36 +0000

    Task 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add -adds two integers and returns the result
+ *
+ * @i: input value
+ * @j: input value
+ *
+ * Return: sum
+ *
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
