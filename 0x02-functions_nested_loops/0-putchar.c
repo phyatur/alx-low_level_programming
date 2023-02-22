caf739e129a70719274cfb659d44e274482a17ee
phyatur <phyatur.official@gmail.com>
2023-02-22 01:06:04 +0000

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - prints _putchar, followed by a new line.
- * 
- * return 0 
- */
+* main - prints _putchar, followed by a new line.
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 	char t[] = "_putchar";
 
-	for (i = 0; i <= 7; i++ )
-	{
-	       _putchar(p[i]);
-	}
-	_putchar('\n');
+	for (i = 0; i <= 7; i++)
+		{
+		_putchar(t[i]);
+		}
+		_putchar('\n');
 
-	return 0;
+	return (0);
 }
