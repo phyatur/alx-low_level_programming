14702b17f9e03837b3b23943bd4bd73be11e3619
phyatur <phyatur.official@gmail.com>
2023-02-22 04:41:26 +0000

    Task 9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+
+			if (j == 0)
+			{
+				_putchar('0' + k);
+			}
+			else if (k <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + k);
+			}
+			else if (k > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (k / 10));
+				_putchar('0' + (k % 10));
+			}
+		}
+		_putchar('\n');
+		}
+}
