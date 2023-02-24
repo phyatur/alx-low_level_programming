fe934ed66e5a61b8b6566ff3ee25bcaa52046ce1
phyatur <phyatur.official@gmail.com>
2023-02-24 01:08:44 +0000

    Task 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers
+ *
+ * @a: input parameter
+ * @b: input parameter
+ *
+ * Return: a * b 
+ */
+
+int mul(int a, int b)
+{	
+	return (a * b);
+}
