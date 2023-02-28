903f3e6f64655de91df3fe642910c93faf37f32a
phyatur <phyatur.official@gmail.com>
2023-02-28 02:19:07 +0000

    Task 8. Arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ *
+ * @a: input arguments
+ * @n: input arguments
+ */
+
+void print_array(int *a, int n)
+{
+	int z;
+
+	for (z = 0; z < n; z++)
+	{
+		printf("%d", a[z]);
+			if (z != n - 1)
+			{
+				printf(", ");
+			}
+	}
+	printf("\n");
+}
+
