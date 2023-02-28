430f1f53caec1fe3f588a8c49b97486d3ce41e66
phyatur <phyatur.official@gmail.com>
2023-02-28 00:31:23 +0000

    Task 0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer and updates the value
+ *
+ * @n: argument as a pointer
+ */
+
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}
