f51b02f51e5e493aca0370b573a3bfd290f70198
phyatur <phyatur.official@gmail.com>
2023-02-28 01:51:19 +0000

    Task 6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: input string argument
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
