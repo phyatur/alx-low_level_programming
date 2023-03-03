35964aa366c220ce5524ec84c5da5acf1f3cd92a
phyatur <phyatur.official@gmail.com>
2023-03-03 05:04:41 +0000

    Task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,35 +10,19 @@
 
 char *leet(char *s)
 {
-	int i = 0;
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 97 || s[i] == 65)
+		for (j = 0; j < 10; j++)
 		{
-			s[i] = 52;
+			if (s[i] == s1[j])
+			{
+				s[i] = s2[j];
+			}
 		}
-		else if (s[i] == 101 || s[i] == 69)
-		{
-			s[i] = 51;
-		}
-		else if (s[i] == 111 || s[i] == 79)
-		{
-			s[i] = 48;
-		}				
-		else if (s[i] == 116 || s[i] == 84)
-		{
-			s[i] = 55;
-		}
-		else if (s[i] == 108 || s[i] == 76)
-		{
-			s[i] = 49;
-		}
-		else
-		{
-			s[i] = s[i];
-		}
-		i++;
 	}
 	return (s);
-}		
+}
