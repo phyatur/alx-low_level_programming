8e9955ebdeb1ae5cffb8f4edc5e96893497ecd0d
phyatur <phyatur.official@gmail.com>
2023-03-03 02:53:05 +0000

    Task 5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes all lowercase to uppercase
+ * 
+ * @str: argument
+ *
+ * Return: char str[i]
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] -32;
+		}
+		i++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -10,5 +12,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
 
 #endif
