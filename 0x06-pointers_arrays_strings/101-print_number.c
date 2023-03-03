3011ff33795c8503dfc0eee207b5cd3b70b58225
phyatur <phyatur.official@gmail.com>
2023-03-03 04:34:42 +0000

    Task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ *
+ * @n: integer argument
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int num1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num1 = -num1;
+	}
+
+	if ((num1 / 10) > 0)
+		print_number(num1 / 10);
+
+	_putchar((num1 % 10) + '0');
+}
