a4e3986313a92e747ee19782a467c13416796513
phyatur <phyatur.official@gmail.com>
2023-03-07 01:33:49 +0000

    Task 2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,11 +14,14 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0' && s[i] != c)
 	{
-		if (s[i] == c)
-			return (&s[i]);
 		i++;
+	}
+
+	if (s[i] == c)
+	{
+	return (&s[i]);
 	}
 	return (00);
 }
