b6bbc682f79b11bfa732d86d5ba178b759cc1c9a
phyatur <phyatur.official@gmail.com>
2023-03-09 23:00:10 +0000

    Task 6. Inception. Is it possible?

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - a function that returns the natural square root
+ *
+ * @n: int number
+ *
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return ();
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * prime_number - a function that returns 1 if the input integer is a prime number
+ * otherwise return 0.
+ *
+ * @n: integer number
+ *
+ * @div: number dividing
+ *
+ * Return: 1 if the input integer is a prime number
+ * 0 otherwise
+ */
+
+int prime(int n, int div)
+{
+	if (n == div)
+		return (1);
+	if (n % div == 0)
+		return (0);
+	return (prime(n, div + 1));
+
+}
+
+/**
+ * is_prime_number - a function that checks if the input integer is a prime number
+ *
+ * @n: integer number
+ *
+ * Return: 1 if the input integer is a prime number
+ * 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n % 2 == 0 || n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+
+	return (prime(n, div));
+}
