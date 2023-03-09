18f1f0da606a02c3822955c7e071aca5717017ea
phyatur <phyatur.official@gmail.com>
2023-03-09 20:39:34 +0000

    Task 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line
+ *
+ * @s: string
+ *
+ * Return: nothing(void)
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
