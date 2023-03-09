6e8f832811da5651af216c234437813273c32533
phyatur <phyatur.official@gmail.com>
2023-03-09 21:15:16 +0000

    Task 4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - a function that returns the value of x raised to the power of y
+ * _pow_recursion - a function that returns x raised to the power of y
  *
  * @x: the int value
  * @y: the int power
