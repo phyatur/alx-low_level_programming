c09a9540a679818b8d5cb9de91483b6af274b990
phyatur <phyatur.official@gmail.com>
2023-03-09 20:54:35 +0000

    Task 2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that returns the length of a string
+ *
+ * @s: string
+ *
+ * Return: nothing(void)
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+
+	return (i);
+}
+
