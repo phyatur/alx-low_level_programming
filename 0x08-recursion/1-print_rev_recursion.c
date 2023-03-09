3f927b354e9053423cec37ac33f148802d4438a8
phyatur <phyatur.official@gmail.com>
2023-03-09 20:48:46 +0000

    Task 1. Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Write a function that prints a string in reverse
+ *
+ * @s: string
+ *
+ * Return: nothing(void)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
