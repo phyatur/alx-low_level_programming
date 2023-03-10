8ed433c0a45f11b7048d9f2a231dc197b4fdc1b6
phyatur <phyatur.official@gmail.com>
2023-03-10 21:19:01 +0000

    Task 1. Silence is argument carried out by other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%i\n", argc-1);
+	return (0);
+}
