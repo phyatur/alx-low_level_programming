3a2d5c6259a695b1317eb82532b5e628c4658fc2
phyatur <phyatur.official@gmail.com>
2023-03-10 21:48:13 +0000

    Task 3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error \n");
+		return (1);
+	}
+	else
+	{
+		int i = atoi(argv[1]);
+		int j = atoi(argv[2]);
+		printf("%i\n", i*j);
+	}
+	return (0);
+}
