3ff7ef431bb74d48d6c9ee23f70dbd80b4dd616c
phyatur <phyatur.official@gmail.com>
2023-03-17 21:54:44 +0000

    Task 0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ *
+ * @b: memory size to be allocated
+ *
+ * Return : a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a = malloc(b);
+
+	if (a == NULL){
+		exit(98);
+	}
+
+	 return (a);
+}
