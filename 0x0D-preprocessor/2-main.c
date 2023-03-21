ff1210c1132874b09668240a8c2dfbd4d398b4ac
phyatur <phyatur.official@gmail.com>
2023-03-21 17:57:53 +0000

    Task 2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - a function that prints the name of the file it was compiled from
  *
  * Return: 0
  */
 
 int main(void)
 {
-	printf("%s \n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
