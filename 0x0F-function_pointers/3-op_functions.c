dee6814bf89e7e6237e82d7763117cf02cea2e7b
phyatur <phyatur.official@gmail.com>
2023-03-22 22:43:33 +0000

    Task 3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+
+/**
+ * op_add - a function that returns the sum of a and b
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_add - a function that returns the difference of a and b
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function that returns the product of a and b
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function that returns the division of a and b
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The quotient of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a function that returns the remainder of the division of a and b
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The remainder of the division of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
