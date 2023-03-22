558f8c042de6fefb562dd2c1105c5e697a0fe1c5
phyatur <phyatur.official@gmail.com>
2023-03-22 21:22:35 +0000

    Task 1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array != NULL && size > 0 & action != NULL)
 	{
-		size_t i;
+		unsigned int i;
 
 		for (i = 0; i < size; i++)
 		{
