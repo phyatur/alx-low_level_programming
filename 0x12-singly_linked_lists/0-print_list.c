b444728553c5d65336781491fc41fe3a3d26c9b1
phyatur <phyatur.official@gmail.com>
2023-03-31 03:50:41 +0000

    Task 0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all elements of a list_t list
+ *
+ * @h: singly linked list to print
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
