918e0264aa164852f5b8964c8be672971f63b1da
phyatur <phyatur.official@gmail.com>
2023-04-04 19:04:54 +0000

    Task 2. Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - a functions that adds a new node at the beginning of a listint_t list
+ * add_nodeint - a fxn that adds a newnode at the beginning of a listint_t list
  *
  * @head: head of the list
  *
