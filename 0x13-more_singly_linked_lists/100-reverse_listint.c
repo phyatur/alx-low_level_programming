0768d2c22498f71a5edc8e01cbfcee117619e3c1
phyatur <phyatur.official@gmail.com>
2023-04-04 19:54:33 +0000

    Task 11. Reverse list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - a function that reverses a listint_t linked list
+ *
+ * @head: head of the list
+ *
+ * Return: pointer to the first node of the reversed list.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *ahead, *behind;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	behind = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		ahead = (*head)->next;
+		(*head)->next = behind;
+		behind = *head;
+		*head = ahead;
+	}
+
+	(*head)->next = behind;
+
+	return (*head);
+}
