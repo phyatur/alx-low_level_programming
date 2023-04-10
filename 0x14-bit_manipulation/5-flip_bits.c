a745509f478245d0554f059458ba0162418c15d1
phyatur <phyatur.official@gmail.com>
2023-04-10 23:30:12 +0000

    Task 5. 101

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - a function that returns the number of bits you would need to flip to get from one number to another.
+ * flip_bits - a function that return the number of bits you would need to flip
  *
  * @n: start number
  *
