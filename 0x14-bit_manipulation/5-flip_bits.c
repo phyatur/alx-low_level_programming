19ce4eeec5b62672f9ed424e43e3f62c3a79bea5
phyatur <phyatur.official@gmail.com>
2023-04-10 23:29:01 +0000

    Task 5. 101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - a function that returns the number of bits you would need to flip to get from one number to another.
+ *
+ * @n: start number
+ *
+ * @m: end number
+ *
+ * Return: the number of bits you would need to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int j, result;
+
+	result = 0;
+	for (j = 8 * sizeof(n) - 1; j >= 0; j--)
+		if (((n ^ m) >> j) & 1)
+			result++;
+	return (result);
+}
