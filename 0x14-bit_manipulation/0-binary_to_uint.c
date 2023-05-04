6056a942621cf3ceb83fd5f9ef5a9c0c2e123e33
phyatur <phyatur.official@gmail.com>
2023-05-04 22:54:06 +0000

    Task 0. 0 - redo

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,11 +18,12 @@ unsigned int binary_to_uint(const char *b)
 	if (!b)
 		return (0);
 
-	while ((b[i] < '0') || (b[i] > '1'))
+	for (i = 0; b[i]; i++)
 	{
-		number <<= 1;
-		number += b[i] - '0';
-		i++;
+		if ((b[i] < '0') || (b[i] > '1'))
+			return (0);
+
+		number = 2 * number + (b[i] - '0');
 	}
 
 	return (number);
