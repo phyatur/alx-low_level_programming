2441918dbdafcefae1906b760af6c75533ca71c1
phyatur <phyatur.official@gmail.com>
2023-07-07 16:48:45 +0000

    Task 6(i)

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -15,13 +15,13 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		node = ht->array[cont];
 		while (node != NULL)
-			{
-				temp = node;
-				node = node->next;
-				free(temp->key);
-				free(temp->value);
-				free(temp);
-			}
+		{
+			temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
 	}
 	free(ht->array);
 	free(ht);
